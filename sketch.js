913e4b161f6b8ccca77ace5659e4a013aca8eb8e
keerthanaslesser <keerthanaslesser29@gmail.com>
2020-11-28 13:00:44 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-
+var plat;
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
@@ -12,6 +12,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    plat = new Ground(200,200,200,200);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -54,4 +55,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    plat.display();
 }
